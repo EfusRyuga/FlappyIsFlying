378da6a1cb077ab32fe9caac805c1b0eec48231b
Efus <57728213+EfusRyuga@users.noreply.github.com>
2021-09-17 10:07:11 +0200

    Update flappy.py

--- jeu/flappy.py
+++ jeu/flappy.py
@@ -1,3 +1,4 @@
 # ce code contient ma version du jeu flappy bird, il reprend les bases du dit jeu.
 # création du repository : 15/09/21  finalistion du projet : X/X/X
 # temps passé sur le projet : Xh ->  2h
+
