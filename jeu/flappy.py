06b6f4e2424e5569d68f2ce8d24670f578dd3591
Efus <57728213+EfusRyuga@users.noreply.github.com>
2021-09-19 19:27:54 +0200

    Update flappy.py

--- jeu/flappy.py
+++ jeu/flappy.py
@@ -1,6 +1,6 @@
 # ce code contient ma version du jeu flappy bird, il reprend les bases du dit jeu.
 # création du repository : 15/09/21  finalistion du projet : X/X/X
-# temps passé sur le projet : Xh ->  2h + 2h + 3h30
+# temps passé sur le projet : Xh ->  2h + 2h + 3h30 + 46min
 
 import pygame, sys, random 
 class game:
