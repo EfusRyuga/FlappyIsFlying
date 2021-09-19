fb9e64a119c9eb09cdfc240094600eefe2ad2b26
Efus <57728213+EfusRyuga@users.noreply.github.com>
2021-09-19 19:24:44 +0200

    Add files via upload

--- jeu/flappy.py
+++ jeu/flappy.py
@@ -1,41 +1,260 @@
 # ce code contient ma version du jeu flappy bird, il reprend les bases du dit jeu.
 # création du repository : 15/09/21  finalistion du projet : X/X/X
-# temps passé sur le projet : Xh ->  2h + 2h
+# temps passé sur le projet : Xh ->  2h + 2h + 3h30
 
-import pygame
-from pygame.locals import * 
-import sys
-import random
+import pygame, sys, random 
+class game:
+	def formation_sol():
+		screen.blit(floor_surface,(sol_x_pos,900))
+		screen.blit(floor_surface,(sol_x_pos + 576,900))
+
+
+
+	def check_col(pipes):
+		global can_score
+		for pipe in pipes:
+			if bird_rect.colliderect(pipe):
+				death_sound.play()
+				can_score = True
+				return False
+
+		if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+			can_score = True
+			return False
+
+		return True
+	
+	def afficher_score(game_state):
+		if game_state == 'main_game':
+			score_surface = game_font.render(str(int(score)),True,(255,255,255))
+			score_rect = score_surface.get_rect(center = (288,100))
+			screen.blit(score_surface,score_rect)
+		if game_state == 'game_over':
+			score_surface = game_font.render(f'Score: {int(score)}' ,True,(255,255,255))
+			score_rect = score_surface.get_rect(center = (288,100))
+			screen.blit(score_surface,score_rect)
+
+			high_score_surface = game_font.render(f'High score: {int(high_score)}',True,(255,255,255))
+			high_score_rect = high_score_surface.get_rect(center = (288,850))
+			screen.blit(high_score_surface,high_score_rect)
+
+	def update_score(score, high_score):
+		if score > high_score:
+			high_score = score
+		return high_score
+
+class flappy:
+
+	def effet_grav(bird):
+		new_bird = pygame.transform.rotozoom(bird,-bird_movement * 4, 2) #4 = vitesse de rota et 1 = image scale
+		return new_bird
+
+	def flappy_frames():
+		new_bird = bird_frames[bird_index]
+		new_bird_rect = new_bird.get_rect(center = (100,bird_rect.centery))
+		return new_bird,new_bird_rect
+
+class tuyaux:
+	def score_verif():
+		global score, can_score 
+		
+		if pipe_list:
+			for pipe in pipe_list:
+				if 95 < pipe.centerx < 105 and can_score: # reste à définir l'utilité des bornes
+					score += 1
+					score_sound.play()
+					can_score = False
+				if pipe.centerx < 0: # vérification de l'allignement de flappy avec les tuyaux
+					can_score = True
+	def crea_tuyaux():
+		random_pipe_pos = random.choice(pipe_height)
+		bottom_pipe = pipe_surface.get_rect(midtop = (700,random_pipe_pos))
+		top_pipe = pipe_surface.get_rect(midbottom = (700,random_pipe_pos - 300))
+		return bottom_pipe,top_pipe
+
+	def deplacement_tuyaux(pipes):
+		for pipe in pipes:
+			pipe.centerx -= 5
+		visible_pipes = [pipe for pipe in pipes if pipe.right > -50]
+		return visible_pipes
+
+	def afficher_tuyaux(pipes):
+		for pipe in pipes:
+			if pipe.bottom >= 1024: 
+				screen.blit(pipe_surface,pipe)
+			else:
+				flip_pipe = pygame.transform.flip(pipe_surface,False,True)
+				screen.blit(flip_pipe,pipe)
+
 pygame.init()
+screen = pygame.display.set_mode((576,1024))
+clock = pygame.time.Clock()
 pygame.display.set_caption('Flappy is flying')
 PI = pygame.image.load('./Assets/interface/favicon.ico')
 pygame.display.set_icon(PI)
-fenetre=pygame.display.set_mode((288, 512)) # taile de la fenetre
-background=pygame.image.load('./Assets/background/background-day.png') #atttribution d'une image à background
+game_font = pygame.font.Font('font.ttf',40)
 
+# var
+gravity = 0.35
+bird_movement = 0
+game_active = True
+score = 0
+high_score = 0
+can_score = True
+randbg = random.choice(['./Assets/background/background-day.png','./Assets/background/background-night.png'])
+bg_surface = pygame.image.load(randbg).convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
 
-#sol
-sol = pygame.image.load('./Assets/background/base.png') #attribution d'une image à sol
+floor_surface = pygame.image.load('./Assets/background/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
 sol_x_pos = 0
-sol = pygame.transform.scale2x(sol) #augmentation de la taille de l'image du sol, question graphique
-def formation_sol():
-	fenetre.blit(sol, (sol_x_pos,400)) #on affiche sol avec x = 0 (mais change plus tard) et y = 400
-	
-#flappy
-class flappy:
-    def __init__(self):
-        skin = './Assets/playerskin/bluebird-downflap.png'
-        self.pygame.image.load(skin)
-	
-while True:
-    for event in pygame.event.get():
-        if event.type==KEYUP : fenetre=pygame.quit()
-        fenetre.blit(background, (0,0)) #affichage du fond
-        formation_sol() #on lance la fonction sol
-        sol_x_pos = sol_x_pos - 1 # on change la valeur de la position du sol pour le faire défiler
-        if sol_x_pos <= -380: # vérification du défilement de l'image pour que le sol forme un espèce de gif
-            sol_x_pos = 0     # (sinon le sol finit par disparaitre)
-        pygame.time.delay(1)   # rafraichissement de l'image toute les secondes 
-        pygame.display.flip()  # //
+
+ #changement skin
+PlayerSkin = random.choice(['bluebird','redbird','yellowbird'])
+if PlayerSkin == 'bluebird':
+    bird_downflap = pygame.image.load('./Assets/playerskin/bluebird-downflap.png').convert_alpha()
+    bird_midflap = pygame.image.load('./Assets/playerskin/bluebird-midflap.png').convert_alpha()
+    bird_upflap = pygame.image.load('./Assets/playerskin/bluebird-upflap.png').convert_alpha()
+elif PlayerSkin == 'redbird':
+    bird_downflap = pygame.image.load('./Assets/playerskin/redbird-downflap.png').convert_alpha()
+    bird_midflap = pygame.image.load('./Assets/playerskin/redbird-midflap.png').convert_alpha()
+    bird_upflap = pygame.image.load('./Assets/playerskin/redbird-upflap.png').convert_alpha()
+elif PlayerSkin == 'yellowbird':
+	bird_downflap = pygame.image.load('./Assets/playerskin/yellowbird-downflap.png').convert_alpha()
+	bird_midflap = pygame.image.load('./Assets/playerskin/yellowbird-midflap.png').convert_alpha()
+	bird_upflap = pygame.image.load('./Assets/playerskin/yellowbird-upflap.png').convert_alpha()
 
 
+bird_frames = [bird_downflap,bird_midflap,bird_upflap]
+bird_index = 0
+bird_surface = bird_frames[bird_index]
+bird_rect = bird_surface.get_rect(center = (100,512))
+
+BIRDFLAP = pygame.USEREVENT + 1
+pygame.time.set_timer(BIRDFLAP,200)
+
+rand_tuyaux = random.choice(['./Assets/obstacleskin/pipe-green.png','./Assets/obstacleskin/pipe-red.png'])
+pipe_surface = pygame.image.load(rand_tuyaux)
+pipe_surface = pygame.transform.scale2x(pipe_surface)
+pipe_list = []
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE,1200)
+pipe_height = [400,600,800]
+
+game_over_surface = pygame.transform.scale2x(pygame.image.load('./Assets/interface/start2.png').convert_alpha())
+game_over_rect = game_over_surface.get_rect(center = (288,512))
+rnd_flap_sound = random.choice(['./Assets/audio/wing.wav','./Assets/audio/swoosh.wav'])
+flap_sound = pygame.mixer.Sound(rnd_flap_sound)
+death_sound = pygame.mixer.Sound('./Assets/audio/hit.wav')
+score_sound = pygame.mixer.Sound('./Assets/audio/point.wav')
+score_sound_countdown = 100
+SCOREEVENT = pygame.USEREVENT + 2
+pygame.time.set_timer(SCOREEVENT,100)
+
+while True: # pour toujours, vérification d'appuie de touche
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE and game_active:
+				bird_movement = 0
+				bird_movement -= 12
+				flap_sound.play()
+
+			if event.key == pygame.K_SPACE and game_active == False:
+				game_active = True
+				pipe_list.clear()
+				bird_rect.center = (100,512)
+				bird_movement = 0
+				score = 0
+			if event.key == pygame.K_h and game_active == False:
+				print("welcome to menu, here, you will get some help \n  all command, if pressed on menu, will change some parameters : \n y -> flappy is yellow \n r -> flappy is red \n b -> flappy is blue")
+			if event.key == pygame.K_y and game_active == False:
+				bird_downflap = pygame.image.load('./Assets/playerskin/yellowbird-downflap.png').convert_alpha()
+				bird_midflap = pygame.image.load('./Assets/playerskin/yellowbird-midflap.png').convert_alpha()
+				bird_upflap = pygame.image.load('./Assets/playerskin/yellowbird-upflap.png').convert_alpha()
+				bird_frames = [bird_downflap,bird_midflap,bird_upflap]
+				bird_index = 0
+				bird_surface = bird_frames[bird_index]
+				bird_rect = bird_surface.get_rect(center = (100,512))
+				print("flappy is now yellow")
+			if event.key == pygame.K_b and game_active == False:
+				bird_downflap = pygame.image.load('./Assets/playerskin/bluebird-downflap.png').convert_alpha()
+				bird_midflap = pygame.image.load('./Assets/playerskin/bluebird-midflap.png').convert_alpha()
+				bird_upflap = pygame.image.load('./Assets/playerskin/bluebird-upflap.png').convert_alpha()
+				bird_frames = [bird_downflap,bird_midflap,bird_upflap]
+				bird_index = 0
+				bird_surface = bird_frames[bird_index]
+				bird_rect = bird_surface.get_rect(center = (100,512))
+				print("flappy is now blue")
+			if event.key == pygame.K_r and game_active == False:
+				bird_downflap = pygame.image.load('./Assets/playerskin/redbird-downflap.png').convert_alpha()
+				bird_midflap = pygame.image.load('./Assets/playerskin/redbird-midflap.png').convert_alpha()
+				bird_upflap = pygame.image.load('./Assets/playerskin/redbird-upflap.png').convert_alpha()
+				bird_frames = [bird_downflap,bird_midflap,bird_upflap]
+				bird_index = 0
+				bird_surface = bird_frames[bird_index]
+				bird_rect = bird_surface.get_rect(center = (100,512))
+				print("flappy is now red")
+			if event.key == pygame.K_RIGHT and game_active == False:
+				pipe_surface = pygame.image.load('./Assets/obstacleskin/pipe-red.png')
+				pipe_surface = pygame.transform.scale2x(pipe_surface)
+				print("pipe is now red")
+			if event.key == pygame.K_LEFT and game_active == False:
+				pipe_surface = pygame.image.load('./Assets/obstacleskin/pipe-green.png')
+				pipe_surface = pygame.transform.scale2x(pipe_surface)
+				print("pipe is now green")
+
+		if event.type == SPAWNPIPE:
+			pipe_list.extend(tuyaux.crea_tuyaux()) #ajout d'un tuyau à la liste
+
+		if event.type == BIRDFLAP: #fait tourner les frames de flappy pour créer son animation de vol
+			if bird_index < 2:
+				bird_index += 1
+			else:
+				bird_index = 0
+
+			bird_surface,bird_rect = flappy.flappy_frames()
+
+	screen.blit(bg_surface,(0,0)) #affiche le fond d'écran
+
+	if game_active: #vérifie le state "game active" -> n'est pas actif si dans le menu
+
+		# Bird
+		bird_movement += gravity #ajout de la variable gravité au déplacement de flappy
+		rotated_bird = flappy.effet_grav(bird_surface) # ajout de l'effet gravité au skin de flappy
+		bird_rect.centery += bird_movement # ?????????
+		screen.blit(rotated_bird,bird_rect) # affichage du skin sous gravité
+		game_active = game.check_col(pipe_list) #vérification des collisions 
+			
+		# Pipes
+		pipe_list = tuyaux.deplacement_tuyaux(pipe_list) #déplacement des tuyaux présent dans la liste de tuyaux
+		tuyaux.afficher_tuyaux(pipe_list) #affichage des dits tuyaux
+
+		#sol
+		sol_x_pos -= 5 #sol ig -> même vitesse que tuyaux pour que ça ne file pas la gerbe
+		game.formation_sol() # appel de la méthode formation-sol() dans la class game 
+		if sol_x_pos <= -576: # création d'une loop pour que le sol ne disparaisse jamais
+			sol_x_pos = 0 #réinitialisation de la position du sol
+		
+		# Score
+		tuyaux.score_verif() #vérification du score (si le score = score +1)
+		game.afficher_score('main_game') #affichage du score au state game state = main_game
+	else: # si dans le menu
+		screen.blit(game_over_surface,game_over_rect)
+		high_score = game.update_score(score,high_score)
+		game.afficher_score("game_over") #affichage du score au state game state = game_over
+
+		#sol
+		sol_x_pos -= 1 # vitesse lente pour avoir un menu chill
+		game.formation_sol() # appel de la méthode formation-sol() dans la class game 
+		if sol_x_pos <= -576: # création d'une loop pour que le sol ne disparaisse jamais
+			sol_x_pos = 0 #réinitialisation de la position du sol
+
+	# Floor
+	
+	
+
+	pygame.display.update() # créer l'image
+	clock.tick(100) # définit le nombre de fps plus le chiffre est grand plus le rafraichissement est régulier
